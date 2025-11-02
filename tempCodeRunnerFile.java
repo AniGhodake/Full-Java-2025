e293ca6a1659a75e467e742bf8199a82526d5a3a
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-11-03 04:19:04 +0530

    First Commit

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+ Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the Second Number : ");
+        int num2 = sc.nextInt();
