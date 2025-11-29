57d685b30b47e24e61712eeac3100ebeb0e0d7ad
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-11-29 10:02:07 +0530

    4th December 2025

--- SumAndAverageArray.java
+++ SumAndAverageArray.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class SumAndAverageArray{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+       // Scanner sc = new Scanner(System.in);
         int arr[] = userArray();
 
         // int i = 0;
