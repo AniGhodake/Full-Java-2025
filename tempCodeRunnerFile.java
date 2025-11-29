f27fb1ba082b3785277ff1c341e40ca17234e7dc
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-11-29 10:02:15 +0530

    4th December 2025

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,5 +1,1 @@
- Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the First Number : ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter the Second Number : ");
-        int num2 = sc.nextInt();
+
